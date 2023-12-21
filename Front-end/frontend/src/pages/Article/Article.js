882b67e45adb389f30c1d43530a2e1985e318a6a
fadi <fadi@codi.tech>
2023-12-21 13:57:09 +0200

    Added conditional rendering function with no data found

--- Front-end/frontend/src/pages/Article/Article.js
+++ Front-end/frontend/src/pages/Article/Article.js
@@ -14,7 +14,6 @@ const Article = () => {
       try{
      const article = await axios.get(`http://localhost:5000/article/${id}`)
      if(article){
-      console.log(article)
      setArticle(article.data.data)
      setisLoading(false)
      }
@@ -24,7 +23,6 @@ const Article = () => {
      }
     }
       catch(err){
-        console.log(err)
         setisLoading(false)
 
       }
@@ -32,28 +30,33 @@ const Article = () => {
     fetchArticle();
   }
   ,[])
+
+
+  const renderContent = () => {
+    if (isLoading) return <div className={Styles.container}><img src={loader} alt="loading" /></div>;
+    // if (isError) return <Error />;
+    if (article && !isLoading) return ( <article className={Styles.articleContainer}>
+    <img 
+    alt={article.title}
+    src={article.imageURL}/>
+    <div className={Styles.articleWrapper}>
+      <header>
+        <h1>{article.title}</h1>
+        <p><span>by </span>{`${article.Author.firstName} ${article.Author.lastName}`}</p>
+        <time>{article.createdAt.split("T")[0]}</time>
+      </header>
+      <section className={Styles.articleDetails}>
+      {article.body}
+      </section>
+    </div>
+  </article>);
+
+    // This is the default component if none of the conditions above are met
+    return <div className={Styles.container}><h1>No Article found</h1></div>;
+  };
   
   return (
-  <>
-    {
-    
-    (!isLoading && article) ? (
-      <article className={Styles.articleContainer}>
-        <img 
-        alt={article.title}
-        src={article.imageURL}/>
-        <div className={Styles.articleWrapper}>
-          <header>
-            <h1>{article.title}</h1>
-            <p><span>by </span>{`${article.Author.firstName} ${article.Author.lastName}`}</p>
-            <time>{article.createdAt.split("T")[0]}</time>
-          </header>
-          <section className={Styles.articleDetails}>
-          {article.body}
-          </section>
-        </div>
-      </article>)  : <img src={loader} alt="loading" />}
-      </>
+    renderContent()
   )
 }
 
